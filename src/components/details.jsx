0274e60af2b825a293429288b87fbed4bf80ba61
Nestavic <hopeirungu0104@gmail.com>
2025-06-05 17:23:13 +0300

    Updated details component

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -4,7 +4,7 @@ import wel from '../assets/images/wel.gif'
 const details = () => {
   return (
     <div>
-      <h3>All Categories Of Baby Accessories Are Available</h3>
+      <h3>All Categories Of Baby Accessories Are Available Here</h3>
       <img src={wel} alt="" />
     </div>
   )
